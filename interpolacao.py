d9d5b7f3ad230f2cc126b491654d8f669ef3d6d2
Larissa Hirai <larissa.hirai@mail.uft.edu.br>
2023-03-25 20:39:48 -0300

    Base para codificação

---
+++ interpolacao.py
@@ -0,0 +1,33 @@
+"""
+Universidade Federal do Tocantins - Campus Palmas
+Discilina: Processamento de Imagens
+Professora: Glenda Botelho
+Alunas: Fernanda Plessim e Larissa Hirai
+Curso: Ciêmcias da Computação
+"""
+# Atividade 1
+# Biblioteca numpy está sendo importada
+import numpy as np
+# Bibliotecas PIL e matplotlib sendo importadas
+from PIL import Image
+from matplotlib import image
+from matplotlib import pyplot
+"""
+Para baixar as bibliotecas basta colocar o seguinte código no terminal python do VsCode:
+pip install numpy
+pip install matplotlib
+(Com esse comando a biblioteca PIL vai ser instalada no mesmo pacote)
+"""
+# Primeiramente transformar imagem colorida em tons de cinza
+# abre a imagem colorida
+imagem = Image.open('imagem_colorida.jpeg')
+
+# converte a imagem para o modo L (escala de cinza)
+img_escala_de_cinza = imagem.convert('L')
+
+# salva a nova imagem
+img_escala_de_cinza.save('imagem_tons_cinza.jpeg')
+
+# transforma a imagem em matriz
+data = image.imread('imagem_tons_cinza.jpeg')
+print("Imagem -> Matriz")
